3bbc04dca4b320a171c036c343bc871c1320900a
mahmoudskhairi <mahmoud.skhairi@gmail.com>
2024-03-31 16:39:27 +0000

    second commit

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: mskhairi <mskhairi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 16:14:25 by mskhairi          #+#    #+#             */
-/*   Updated: 2024/03/30 19:47:37 by mskhairi         ###   ########.fr       */
+/*   Updated: 2024/03/31 15:49:04 by mskhairi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,15 @@
 #include <stdio.h>
 #include <signal.h>
 
+int ft_strlen(char *str)
+{
+    int i = 0;
+    while (str[i++]);
+    return (--i);
+}
 
-void send_char(char c, int pid)
+
+void send_char(unsigned char c, int pid)
 {
     int i = 7;
     while (i >= 0)
@@ -34,6 +41,11 @@ void send_char(char c, int pid)
     }
 }
 
+// int main()
+// {
+//     printf("%d\n", ft_strlen(""));
+// }
+
 int main(int ac, char **av)
 {
     if (ac == 3)
@@ -41,13 +53,15 @@ int main(int ac, char **av)
         int pid = atoi (av[1]);
         char *str = av[2];
         int i = 0;
+        int len = ft_strlen(av[2]);
+        printf("len == %d\n", len);
+        send_char(len, pid);
         while (str[i])
         {
             send_char(str[i], pid);
             i++;
         }
-        send_char('\n', pid);
-        // send_len(atoi(av[3]));
+        // send_char('\n', pid);
         // send_char('A', pid);
         //    kill(pid, SIGUSR1);
         printf("the signal is sent");
--- minitalk/server.c
+++ minitalk/server.c
@@ -3,19 +3,76 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int len = -1;
+
+int ft_strlen(char *str)
+{
+    int i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*s;
+
+	s = b;
+	while (len--)
+	{
+		*(unsigned char *)s++ = (unsigned char)c;
+	}
+	return (b);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	bytes;
+	void	*p;
+    int i;
+
+    i = 0;
+	bytes = count * size;
+	if (size && bytes / size != count)
+		return (NULL);
+	p = malloc(bytes);
+	if (p == NULL)
+		return (NULL);
+    ft_memset(p, 0, bytes);
+	return (p);
+}
+
 void handle(int client_bit)
 {
     static int c;
     static int count = 7;
-    char x;
+    static char *string;
+    static int i;
+
     if(client_bit == SIGUSR1)
         c |= ((1 << count));
     count--;
     if (count < 0)
     {
         // c += 48;
-        x = (char)c;
-        write (1, &x, 1);
+        if (len == -1)
+        {
+            len = c;
+            string = ft_calloc (len * sizeof(char) , 1);
+        }
+        else
+        {
+            string[i] = (char)c;
+            i++;
+            if(ft_strlen(string) == len)
+            {
+                write(1, string, ft_strlen(string));
+                free(string);
+                string = NULL;
+                len = -1;
+                i = 0;
+            }
+        }
         count = 7;
         c = 0;
     }
@@ -25,21 +82,12 @@ int main()
 {
     printf("i am a server\n");
     printf("my pid is : %d\n", getpid());
-    signal(SIGUSR1, handle);
-    signal(SIGUSR2, handle);
+    sigaction(SIGUSR1, &handle, NULL);
+    sigaction(SIGUSR2, &handle, NULL);
     // write (1, "dd\n", 3);
     while (1)
     {
         
         pause ();
     }
-
-    // handle (0);
-    // handle (1);
-    // handle (0);
-    // handle (0);
-    // handle (0);
-    // handle (0);
-    // handle (0);
-    // handle (1);
 }
