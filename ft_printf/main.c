8a13c0c372e18702789d61523b837b9d62644a28
mahmoudskhairi <mahmoud.skhairi@gmail.com>
2024-03-31 13:33:52 +0000

    commit

---
+++ ft_printf/main.c
@@ -0,0 +1,10 @@
+#include "ft_printf.h"
+#include <stdio.h>
+
+int main(void)
+{
+    // int x = 2;
+    // ft_printf("%f");
+    // printf("originr :%p\n", 100);
+    // printf("%p\n", (void *)100);
+}
